a11121016a0d0ea6a24e901a2daaed0892cc0e81
Vyacheslav Suslov <aswexxx@gmail.com>
2018-09-03 03:24:33 +0300

    Lesson 6 / init commit

---
+++ Lesson6/src/main/java/ru/vyacheslav/javacoreprof/l6/task1/MainTask1.java
@@ -0,0 +1,9 @@
+package ru.vyacheslav.javacoreprof.l6.task1;
+
+public class MainTask1
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
