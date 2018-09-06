43c556421b206e0a172a3b63dfc4b082f398d50a
Vyacheslav Suslov <aswexxx@gmail.com>
2018-09-06 12:16:00 +0300

    lesson 7 / init commit

---
+++ lesson7/src/main/java/ru/vyacheslav/javacoreprof/l7/lesson7/Application.java
@@ -0,0 +1,9 @@
+package ru.vyacheslav.javacoreprof.l7.lesson7;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello lesson7");
+    }
+
+}
